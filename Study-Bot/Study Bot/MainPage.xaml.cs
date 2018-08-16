7326a2f75e330894ce42a31e302eab09e5b0a549
Winona Azure <v-wiazur@microsoft.com>
2018-08-15 18:46:07 -0700

    Study-Bot initial commit

---
+++ Study-Bot/Study Bot/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Study_Bot
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (rootPivot.SelectedIndex > 0)
+            {
+                // If not at the first item, go back to the previous one.
+                rootPivot.SelectedIndex -= 1;
+            }
+            else
+            {
+                // The first PivotItem is selected, so loop around to the last item.
+                rootPivot.SelectedIndex = rootPivot.Items.Count - 1;
+            }
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (rootPivot.SelectedIndex < rootPivot.Items.Count - 1)
+            {
+                // If not at the last item, go to the next one.
+                rootPivot.SelectedIndex += 1;
+            }
+            else
+            {
+                // The last PivotItem is selected, so loop around to the first item.
+                rootPivot.SelectedIndex = 0;
+            }
+        }
+
+        private void journals_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+
+        }
+    }
+}
