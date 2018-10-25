0a2fe5a9d5c6f21fab9409b8533f89c27344d20c
Winona Azure <v-wiazur@microsoft.com>
2018-10-24 17:47:02 -0700

    Initial commit: using bot v4

---
+++ Qna-Luis-Botv4/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild BasicBot.sln -p:DeployOnBuild=true -p:PublishProfile=studybotcsharp-Web-Deploy.pubxml -p:Password=XwNfnWSo7nEsPASTkD3xACdfNNaG6tn9f6c2NsXGeQ5X4fDbSagkxmoBdhem
+
